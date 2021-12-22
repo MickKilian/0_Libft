9a7e8672decd17f9ca0f459e8c5562cc7a6f9849
Michael BOURGEOIS <mbourgeo@e2r9p12.clusters.42paris.fr>
2021-12-23 00:39:41 +0100

    second push of libft

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,35 +6,11 @@
 /*   By: mbourgeo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 16:20:13 by mbourgeo          #+#    #+#             */
-/*   Updated: 2021/12/21 19:04:11 by mbourgeo         ###   ########.fr       */
+/*   Updated: 2021/12/23 00:30:19 by mbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static	int	ft_isblank(int c)
-{
-	if ((c >= '\t' && c <= '\r') || c == ' ')
-	{
-		return (1);
-	}
-	return (0);
-}
-
-static	int	ft_fullblank(char *s)
-{
-	int	i;
-
-	i = 0;
-	while (*(s + i))
-	{
-		if (!ft_isblank(*(s + i++)))
-		{
-			return (0);
-		}
-	}
-	return (1);
-}
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
@@ -42,8 +18,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	if (s1 == NULL || set == NULL)
 		return (NULL);
-	if (ft_fullblank((char *)s1) || *s1 == '\0')
-		return (ft_strdup(""));
 	length = ft_strlen(s1);
 	while (ft_strchr(set, *(s1 + length - 1)) && length != 0)
 		length--;
